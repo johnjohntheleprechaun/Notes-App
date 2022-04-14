060d57e6e38a6e74daaf196e10d10c327154d48b
John Jorgensen <john@jorgensenfamily.us>
2022-04-13 17:49:26 -0700

    database commands and error matching

--- database.go
+++ database.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
+	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -10,7 +12,8 @@ const userTableCreate = `
 CREATE TABLE IF NOT EXISTS Users (
 	UserID INTEGER PRIMARY KEY,
 	Username TEXT NOT NULL UNIQUE,
-	Password TEXT NOT NULL
+	Password TEXT NOT NULL,
+	AuthToken INTEGER UNIQUE
 )`
 const notesTableCreate = `
 CREATE TABLE IF NOT EXISTS Notes (
@@ -19,6 +22,9 @@ CREATE TABLE IF NOT EXISTS Notes (
 	Content TEXT,
 	FOREIGN KEY(Owner) REFERENCES Users(UserID)
 )`
+const userCreate = "INSERT INTO Users (Username, Password) VALUES (?, ?)"
+const noteCreate = "INSERT INTO Notes (Owner, Content) VALUES (?, ?)"
+const noteEdit = "UPDATE Notes SET Content=? WHERE NoteID=? AND (SELECT AuthToken FROM Users WHERE UserID=?)=?"
 
 func initDB(filepath string) *sql.DB {
 	//os.Remove(filepath)
@@ -27,5 +33,15 @@ func initDB(filepath string) *sql.DB {
 	_, err = db.Exec("PRAGMA foreign_keys = ON")
 	_, err = db.Exec(userTableCreate)
 	_, err = db.Exec(notesTableCreate)
+	fmt.Println(createUser(db, "Johnny_Thunder", "thisisapassword"))
 	return db
+}
+
+func createUser(db *sql.DB, username string, password string) bool {
+	_, err := db.Exec(userCreate, username, password)
+	goodUsername := true
+	if e, ok := err.(sqlite3.Error); ok {
+		goodUsername = (e.ExtendedCode != sqlite3.ErrConstraintUnique)
+	}
+	return goodUsername
 }
