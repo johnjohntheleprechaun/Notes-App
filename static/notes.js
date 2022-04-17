0e9a624ef957cf530b94e48b52dcb2ba8d679cc1
John Jorgensen <john@jorgensenfamily.us>
2022-04-17 00:28:12 -0700

    static bolding (using offset for replacements)

--- static/notes.js
+++ static/notes.js
@@ -14,9 +14,17 @@ const shortcutKeys = {
     },
     "r": function() {
         var results = note.innerHTML.matchAll(/(\*{1,3})([^*]+)\1/g)
+        var offset = 0;
         for (var result of results) {
-            console.log(result);
-            note.innerHTML = replaceRange(note.innerHTML, result.index, result.index + result[0].length, genReplaceString(result[1], result[2]))
+            note.innerHTML = replaceRange(
+                note.innerHTML,
+                result.index + offset,
+                result.index + result[0].length + offset,
+                genReplaceString(result[1], result[2])
+            );
+            var htmlTags = formatCharsToHTML[result[1]];
+            offset += (htmlTags[0].length + htmlTags[1].length) - (result[1].length*2);
+            console.log(htmlTags[0].length);
         }
     }
 }
